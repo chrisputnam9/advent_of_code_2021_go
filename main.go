8ceae992d7c23f0a8b72b80d280c0c64da7528d8
Chris Putnam <chris@webfx.com>
2021-12-30 07:09:37 -0500

    Refactoring in progress

--- main.go
+++ main.go
@@ -9,54 +9,76 @@ import (
 	"errors"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
 const defaultFilename = "input.txt"
 
 // Default day to run
-var day = "day01"
+var day = "day_01"
 
 func main() {
 	day = get_day()
 	log.Print("AoC 2021 - " + day)
 
 	switch day {
-		//case "day01": day01()
-		//case "day01_part2": day01_part2()
+		case "day_01": day_01()
+		case "day_01_part2": day_01(3)
 		default: log.Fatal(day + " is not yet implemented")
 	}
+
+}
+
+/**
+ * Day 1
+ */
+func day_01(window_length_specified ...int) {
+
+	//window_length := 1
+
+	//if len(window_length_specified) > 0 {
+		//window_length = window_length_specified[0]
+	//}
 
 	file := get_input_file()
 	defer file.Close()
 
-	first_line := true
-	var previous float64 = -1 // Probably could be int based on input, but not detailed in specs
-	var increases int = 0
+	increases := 0
+
+	number_list:= make([]float64, 0)
 
 	scanner := bufio.NewScanner(file)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	// Read lines into an array of floats
 	for scanner.Scan() {
-		current, err := strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if ! first_line {
-			if current > previous {
-				increases++
-			}
-		}
-
-		previous = current
-		first_line = false
+		number_list = append(number_list, number)
 	}
 
-	log.Printf("Increases: %d", increases)
+	log.Print(number_list)
 
+	//var previous_window [3]float64
+	//var next_window [3]float64
+
+	log.Printf("Increases: %d", increases)
+}
+
+/**
+ * Get the day from arguments if specified
+ */
+func get_day() string {
+	if len(os.Args) > 1 {
+		day = os.Args[1]
+	}
+	return day
 }
 
 func get_input_file() *os.File {
@@ -76,21 +98,11 @@ func get_input_file() *os.File {
 }
 
 /**
- * Get the day from arguments if specified
- */
-func get_day() string {
-	if len(os.Args) > 1 {
-		day = os.Args[1]
-	}
-	return day
-}
-
-/**
  * Get path from arguments or fall back to defaultFilename
  */
 func get_input_filepath() string {
 	if len(os.Args) < 3 {
-		return defaultFilename
+		return filepath.FromSlash(day + "/" + defaultFilename)
 	}
 	return os.Args[2]
 }
