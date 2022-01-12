835c67c2497be3a9c67950b8b1e462b805b6c9be
Chris Putnam <chris@webfx.com>
2022-01-12 07:13:21 -0500

    Finalize Day 3 part 2

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 const defaultFilename = "input.txt"
@@ -76,42 +78,112 @@ func day_03(calculation string) {
 	}
 
 }
+
 func day_03_life_support(number_list [][]int) {
 
-	oxygen_list := make([][]int, 0)
+	oxygen_list := make([][]int, len(number_list))
 	copy(oxygen_list, number_list)
-	co2_list := make([][]int, 0);
+	co2_list := make([][]int, len(number_list));
 	copy(co2_list, number_list)
 
-	oxygen:= ""
-	co2 := ""
+	log.Printf("Number List Length: %d", len(number_list))
+	log.Printf("Oxygen Length: %d, CO2 Length: %d", len(oxygen_list), len(co2_list))
 
 	// Loop through each position
 	for i := 0; i < len(number_list[0]); i++ {
 
-		zeros, ones := day_03_count_bits(number_list)
-		count_zeros := zeros[i]
-		count_ones := ones[i]
+		log.Printf("----------------------------------------------------------------------------------")
+		log.Printf("Position: %d", i)
 
-		oxygen_bit_keep := 1
-		co2_bit_keep := 0
+		if len(oxygen_list) > 1 {
+			oxygen_list = day_03_life_support_filter(oxygen_list, i, "oxygen")
+		}
+		if len(co2_list) > 1 {
+			co2_list = day_03_life_support_filter(co2_list, i, "co2")
+		}
 
-		// Check for most common bit
-		if ( count_zeros > count_ones ) {
-			oxygen_bit_keep = 0
-			co2_bit_keep = 1
-		} // otherwise, stick with default 1/0
+		if len(oxygen_list) <= 1 && len(co2_list) <= 1 {
+			break
+		}
 
-		// TODO
-		// We could improve this by stopping once we have only one
-		oxygen_list = day_03_life_support_filter(oxygen_list, i, oxygen_bit_keep)
-		co2_list = day_03_life_support_filter(co2_list, i, co2_bit_keep)
-
+		log.Printf("Oxygen Length: %d, CO2 Length: %d", len(oxygen_list), len(co2_list))
 	}
 
-	// Finalize
-	// TODO
+	if len(oxygen_list) < 1 || len(co2_list) < 1 {
+		log.Fatal("Something went wrong - one of the lists is empty - check logic")
+	}
 
+	if len(oxygen_list) > 1 || len(co2_list) > 1 {
+		log.Fatal("Something went wrong - one of the lists is too long - check logic and input data")
+	}
+
+	oxygen := strings.Trim(strings.Replace(fmt.Sprint(oxygen_list[0]), " ", "", -1), "[]")
+	co2 := strings.Trim(strings.Replace(fmt.Sprint(co2_list[0]), " ", "", -1), "[]")
+
+	oxygen_decimal, err := strconv.ParseUint(oxygen, 2, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	co2_decimal, err := strconv.ParseUint(co2, 2, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	multiplied := oxygen_decimal * co2_decimal
+
+	// Output information and multiplied value
+	log.Printf("----------------------------------------------------------------------------------")
+	log.Printf("Oxygen Digits: %v", oxygen_list[0])
+	log.Printf("CO2 Digits: %v", co2_list[0])
+	log.Print("-----------------------------------")
+	log.Printf("Oxygen Binary: %s", oxygen)
+	log.Printf("CO2 Binary: %s", co2)
+	log.Print("-----------------------------------")
+	log.Printf("Oxygen Decimal: %d", oxygen_decimal)
+	log.Printf("CO2 Decimal: %d", co2_decimal)
+	log.Print("-----------------------------------")
+	log.Printf("Multiplied: %d", multiplied)
+
+}
+
+/**
+ * Filter number list based on a given position and what bit signals to keep that number
+ * - eg. for position 1, bit 0 - keep all numbers with a 0 in position 1
+ */
+func day_03_life_support_filter(number_list [][]int, position int, filter_type string) [][]int {
+
+	// We could make this more efficient by only counting at the position we actually need
+	zeros, ones := day_03_count_bits(number_list)
+	count_zeros := zeros[position]
+	count_ones := ones[position]
+
+	bit_to_keep := 1
+	switch filter_type {
+		case "oxygen":
+			if count_zeros > count_ones {
+				bit_to_keep = 0 // otherwise, default is 1
+			}
+		case "co2":
+			if count_zeros <= count_ones {
+				bit_to_keep = 0 // otherwise, if more zeros, we keep 1 (our default in this case)
+			}
+		default:
+			log.Fatal("Invalid filter_type '%s'", filter_type)
+	}
+
+	log.Printf("%s: Zeros: %d, Ones: %d, Bit to Keep: %d", filter_type, count_zeros, count_ones, bit_to_keep)
+
+	filtered := make([][]int, 0)
+
+	for _, number := range number_list {
+		digit := number[position]
+		if digit == bit_to_keep {
+			filtered = append(filtered, number)
+		}
+	}
+
+	return filtered
 }
 
 /**
@@ -138,30 +210,15 @@ func day_03_count_bits(number_list [][]int) ([]int, []int) {
 					ones[i]++
 			}
 
-			log.Printf("Digits: %v, Zeroes: %v, Ones: %v", digit_list, zeros, ones);
+			//log.Printf("Digits: %v, Zeroes: %v, Ones: %v", digit_list, zeros, ones);
 		}
 	}
 
 	return zeros, ones
 }
-/**
- * Filter number list based on a given position and what bit signals to keep that number
- * - eg. for position 1, bit 0 - keep all numbers with a 0 in position 1
- */
-func day_03_life_support_filter(number_list [][]int, position, bit_to_keep int) [][]int {
-	filtered := make([][]int, 0)
 
-	for _, number := range number_list {
-		digit := number[position]
-		if digit == bit_to_keep {
-			filtered = append(filtered, number)
-		}
-	}
-
-	return filtered
-}
 /**
- * Day 3 part 2 - calculate power consumption
+ * Day 3 part 1 - calculate power consumption
  */
 func day_03_power_consumption(number_list [][]int){
 
@@ -273,7 +330,7 @@ func day_02(use_aim_specified ...bool) {
 			log.Fatal("Invalid command: " + command)
 		}
 
-		log.Printf("Command: %s, A: %d, H:%d, D:%d", command, aim, horizontal_position, depth)
+		//log.Printf("Command: %s, A: %d, H:%d, D:%d", command, aim, horizontal_position, depth)
 	}
 
 	// Output information and multiplied value
@@ -339,7 +396,7 @@ func day_01(window_length_specified ...int) {
 			sum2 += number_list[i+s2+1]
 		}
 
-		log.Printf("Sums: %f, %f", sum1, sum2)
+		//log.Printf("Sums: %f, %f", sum1, sum2)
 
 		if sum2 > sum1 {
 			increases++
